1ab4125d46cc547d69479f6730c69ec147803bba
kik1975 <kik@ngs.ru>
2021-03-31 14:19:05 +0700

    Добавление калькулятора для Саймона 2

---
+++ simone2/settings.js
@@ -0,0 +1,9 @@
+var videoBonus = 12;
+var baseStr = ["",","",""];
+var graalStr = ["","","",""];
+var accNames = ["","","",""];
+var accs = 1
+var begDate = new Date(2021, 2, 31);
+var range = 133;
+var isLiteVersion = false;
+var isAutoLevel = true;
