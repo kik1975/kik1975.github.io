2a364334fabb7aa9abf63752812200a9a52c9251
kik1975 <kik@ngs.ru>
2021-04-01 10:28:06 +0700

    Update settings.js

--- kik/settings.js
+++ kik/settings.js
@@ -1,5 +1,5 @@
 var videoBonus = 6;
-var baseStr = ["","","",""];
+var baseStr = ["","87,80,82","",""];
 var graalStr = ["2196,1699,1578,1564,1465","104,98,96,87","68,52,43,36",""];
 var accNames = ["ВКонтакте","Google","Facebook",""];
 var accs = 3
