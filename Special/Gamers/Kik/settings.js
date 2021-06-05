ab07feb11c018395001bbaa96aa737d35221d07e
kik1975 <kik@ngs.ru>
2021-06-05 19:24:32 +0700

    typo

--- Special/Gamers/Kik/settings.js
+++ Special/Gamers/Kik/settings.js
@@ -5,6 +5,6 @@ var graalStr = ["1270,756,540,386,276,983","145,121,93,72,55","126,124,104,87,72
 var accNames = ["ВКонтакте","Google","Facebook",""];
 var accs = 3;
 var begDates = [[2021, 4, 30],[2021, 2, 31],[2021, 4, 23],[2021, 2, 31]];
-var range = [314,133,292,133];
+var range = [315,133,292,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
