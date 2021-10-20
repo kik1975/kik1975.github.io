1a6621f087915eb08e417f868f61379f2f05db26
kik1975 <kik@ngs.ru>
2021-10-20 15:21:24 +0700

    Ruslan

---
+++ Special/Random/Ruslan/settings.js
@@ -0,0 +1,10 @@
+var videoBonuses = [9,12,6,12];
+var code_num = [3,1,3,3];
+var baseStr = ["800,477,341,243,174","","",""];
+var graalStr = ["","","",""];
+var accNames = ["","","",""];
+var accs = 1
+var begDates = [[2021, 9, 20],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
+var range = [266,133,133,133];
+var isLiteVersion = false;
+var isAutoLevel = [true,false,false,false];
