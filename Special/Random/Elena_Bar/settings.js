2a3a1bfe82a46d71b6d65c8203b5db96d7d2ce06
kik1975 <kik@ngs.ru>
2021-10-20 17:10:55 +0700

    Elena_Bar

---
+++ Special/Random/Elena_Bar/settings.js
@@ -0,0 +1,10 @@
+var videoBonuses = [12,9,6,12];
+var code_num = [1,3,3,3];
+var baseStr = ["","","",""];
+var graalStr = ["1438,1028,312,807,942","408,382,328,302,298,125","",""];
+var accNames = ["ВКонтакте","Одноклассники","",""];
+var accs = 2
+var begDates = [[2021, 9, 20],[2021, 9, 20],[2021, 2, 31],[2021, 2, 31]];
+var range = [1665,266,133,133];
+var isLiteVersion = false;
+var isAutoLevel = [true,true,false,false];
