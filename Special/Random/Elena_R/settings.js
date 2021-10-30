ec112c012da8193d201360fe6506fac60a439236
kik1975 <kik@ngs.ru>
2021-10-30 12:03:46 +0700

    Ружанская

---
+++ Special/Random/Elena_R/settings.js
@@ -0,0 +1,10 @@
+var videoBonuses = [9,12,6,12];
+var code_num = [3,1,3,3];
+var baseStr = ["","","",""];
+var graalStr = ["879,523,374,267,191","","",""];
+var accNames = ["","","",""];
+var accs = 1
+var begDates = [[2021, 9, 20],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
+var range = [266,133,133,133];
+var isLiteVersion = false;
+var isAutoLevel = [true,false,false,false];
