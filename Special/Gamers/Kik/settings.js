b3ad7306c6acf2ce64e9f02288b5247428e45dcf
kik1975 <kik@ngs.ru>
2021-11-11 09:33:37 +0700

    Update settings.js

--- Special/Gamers/Kik/settings.js
+++ Special/Gamers/Kik/settings.js
@@ -7,4 +7,4 @@ var videoBonuses = [12,9,7,12];
 var isAutoLevel = [true,true,false,false];
 var begDates = [[2021, 4, 30],[2021, 9, 20],[2021, 4, 23],[2021, 2, 31]];
 var range = [317,266,292,133];
-var code_num = [3,3,3,3];
+var code_num = [3,3,1,3];
