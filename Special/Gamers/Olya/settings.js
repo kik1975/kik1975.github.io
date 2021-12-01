0fe8412bd639022a15bc810630fdd7fe7e7f0a3a
kik1975 <kik@ngs.ru>
2021-12-01 20:38:43 +0700

    без грааля

--- Special/Gamers/Olya/settings.js
+++ Special/Gamers/Olya/settings.js
@@ -1,6 +1,6 @@
 var videoBonuses = [12,12,6,12];
 var code_num = [3,1,3,3];
-var baseStr = ["","","",""];
+var baseStr = ["1539,1209","","",""];
 var graalStr = ["1770,1390,1159","","",""];
 var accNames = ["","","",""];
 var accs = 1
