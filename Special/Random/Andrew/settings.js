9f0583e3234a72c71a39c52c662d83a12dcfda8c
kik1975 <kik@ngs.ru>
2021-12-02 15:39:52 +0700

    Update settings.js

--- Special/Random/Andrew/settings.js
+++ Special/Random/Andrew/settings.js
@@ -1,6 +1,6 @@
 var videoBonuses = [9,12,6,12];
 var code_num = [3,1,3,3];
-var baseStr = ["","","",""];
+var baseStr = ["180,234,252,327,352,457,493,640","","",""];
 var graalStr = ["768,591,549,422,392,302,280,216","","",""];
 var accNames = ["","","",""];
 var accs = 1
