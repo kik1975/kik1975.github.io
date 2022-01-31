c165f3f19e600313ccda4fb9305e4de63c5a08bd
kik1975 <kik@ngs.ru>
2022-01-31 20:26:42 +0700

    Update settings.js

--- Special/Gamers/Fil/settings.js
+++ Special/Gamers/Fil/settings.js
@@ -1,9 +1,9 @@
 var videoBonuses = [12,12,6,12];
-var baseStr = ["","","",""];
-var graalStr = ["719,3311,1971","","",""];
+var baseStr = ["644,902,1262,1767,2968","","",""];
+var graalStr = ["741,1037,1451,2031,3413","","",""];
 var accNames = ["","","",""];
 var accs = 1
-var begDates = [[2021, 4, 30],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [317,133,133,133];
+var begDates = [[2021, 4, 30],[2021, 4, 30],[2021, 2, 31],[2021, 2, 31]];
+var range = [317,317,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
