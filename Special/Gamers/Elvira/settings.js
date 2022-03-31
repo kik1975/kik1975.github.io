fbf11bde782fc24d7b8acdcc60da8b2c4a440ae7
kik1975 <44049549+kik1975@users.noreply.github.com>
2022-03-31 17:58:12 +0700

    Update settings.js

--- Special/Gamers/Elvira/settings.js
+++ Special/Gamers/Elvira/settings.js
@@ -1,9 +1,9 @@
-var videoBonuses = [6,12,6,12];
-var baseStr = ["","","",""];
-var graalStr = ["","","",""];
+var videoBonuses = [12,12,6,12];
+var baseStr = ["616,863,1207,1690,2839","","",""];
+var graalStr = ["709,992,1388,1944,3265","","",""];
 var accNames = ["","","",""];
 var accs = 1
-var begDates = [[2021, 2, 31],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [133,133,133,133];
+var begDates = [[2021, 4, 30],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
+var range = [319,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
