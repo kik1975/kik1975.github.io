707ebcf0dd7dd0130f7a1cf5896c3a9c3cbc160e
kik1975 <44049549+kik1975@users.noreply.github.com>
2022-04-16 13:17:40 +0700

    Update settings.js

--- Special/Random/Andrew/settings.js
+++ Special/Random/Andrew/settings.js
@@ -1,10 +1,10 @@
 var videoBonuses = [12,12,6,12];
-var code_num = [2,1,3,3];
+var code_num = [3,1,3,3];
 var baseStr = ["471,660,923,1292,2171","","",""];
 var graalStr = ["2496,1635,1486,1062,758,542","","",""];
 var accNames = ["","","",""];
 var accs = 1
-var begDates = [[2022, 02, 16],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [1705,133,133,133];
+var begDates = [[2021, 4, 30],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
+var range = [319,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
