dc463d7a368ab1084cafd752ff05235c34506c7c
kik1975 <44049549+kik1975@users.noreply.github.com>
2022-04-24 08:30:01 +0700

    Update settings.js

--- Special/Gamers/Ivan/settings.js
+++ Special/Gamers/Ivan/settings.js
@@ -4,6 +4,6 @@ var graalStr = ["632,884,1238,1732,2910","","",""];
 var accNames = ["","","",""];
 var accs = 1
 var begDates = [[2021, 4, 30],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [319,133,133,133];
+var range = [320,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
