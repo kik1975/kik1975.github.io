2d848ac7655524c7173297497b4b801d9f2d842a
kik1975 <44049549+kik1975@users.noreply.github.com>
2022-04-24 08:30:39 +0700

    Update settings.js

--- Special/Gamers/Olya/settings.js
+++ Special/Gamers/Olya/settings.js
@@ -5,6 +5,6 @@ var graalStr = ["537,751,1051,1471,2472","","",""];
 var accNames = ["","","",""];
 var accs = 1
 var begDates = [[2021, 4, 30],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [319,133,133,133];
+var range = [320,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
