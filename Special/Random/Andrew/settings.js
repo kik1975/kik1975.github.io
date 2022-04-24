900388ba128ab492126f130cf200c70e5133bffb
kik1975 <44049549+kik1975@users.noreply.github.com>
2022-04-24 08:28:29 +0700

    Update settings.js

--- Special/Random/Andrew/settings.js
+++ Special/Random/Andrew/settings.js
@@ -5,6 +5,6 @@ var graalStr = ["2496,1635,1486,1062,758,542","","",""];
 var accNames = ["","","",""];
 var accs = 1
 var begDates = [[2021, 4, 30],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [319,133,133,133];
+var range = [320,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
