8b907269c0c92eef78dff86b5dff278ebac8b995
kik1975 <kik@ngs.ru>
2022-05-23 12:57:41 +0700

    Новый калькулятор множителей

--- MultCalc/calc.js
+++ MultCalc/calc.js
@@ -3,11 +3,20 @@ var mythCounter = 3;
 var onePctCounter = 0;
 var twoPctCounter = 0;
 var SetType = 0;
+var lev_count = [7,7];
+var lev_coeff = [[1.4,1.4168,1.434,1.451,1.468,1.486,1.504],[1.3,1.31,1.32,1.33,1.34,1.35,1.36]];
+var set_coeff = [1.2,1.15];
+var names = ["shapka","parik","predmet","aura"];
+var baseMults = [];
+var graalMults = [];
 
 function Init(){
 	for (var i = 0; i < 2; ++i){
 		accForm.accs[i].hidden = false;
 		accForm.accs[i].addEventListener("click", onclick);
+	}
+	for (var i = 0; i < 4; ++i){
+		document.getElementById(names[i]).max = lev_count[SetType];
 	}
 }
 
@@ -15,6 +24,10 @@ function onclick(e){
 	var newSetType =  parseInt(e.target.value);
 	if (SetType != newSetType)   clear_mults();
     SetType = newSetType;
+	for (var i = 0; i < 4; ++i){
+		document.getElementById(names[i]).value = 1;
+		document.getElementById(names[i]).max = lev_count[SetType];
+	}
 }
 
 function strat() {
@@ -22,91 +35,101 @@ function strat() {
 	let level   = parseFloat(document.getElementById('level').value);
 	let library = parseFloat(document.getElementById('library').value);
 	let graal   = 1.0 + parseFloat(document.getElementById('graal').value)/100.0;
-	let baseMults = [];
-	let graalMults = [];
+	epikCounter   = parseInt(document.getElementById('epikNum').value);
+	mythCounter   = parseInt(document.getElementById('mythNum').value);
+	onePctCounter = parseInt(document.getElementById('oneNum').value);
+	twoPctCounter = parseInt(document.getElementById('twoNum').value);
+	baseMults = [];
+	graalMults = [];
 	let coeff = SetType == 0 ? 1.4 : 1.3;
-	let setCoeff = SetType == 0 ? 1.2 : 1.15;
 	let currMult = rate*level*library*Math.pow(1.05,epikCounter)*
 			Math.pow(1.07,mythCounter)*Math.pow(1.01,onePctCounter)*Math.pow(1.02,twoPctCounter);
-	baseMults[0] = Math.ceil(currMult);
+/*	baseMults[0] = Math.ceil(currMult);
 	graalMults[0] = Math.ceil(currMult*graal);
 	for (var i = 1; i < 4; i++){
 		currMult *= coeff;
 		baseMults[i] = Math.ceil(currMult);
 		graalMults[i] = Math.ceil(currMult*graal);
 	}
-	baseMults[4] = Math.ceil(currMult*coeff*setCoeff);
-	graalMults[4] = Math.ceil(currMult*coeff*setCoeff*graal);
+	baseMults[4] = Math.ceil(currMult*coeff*set_coeff[SetType]);
+	graalMults[4] = Math.ceil(currMult*coeff*set_coeff[SetType]*graal);*/
+	calcMults(currMult,0);
+	baseMults = [...new Set(baseMults)]
+	baseMults.sort(function(a,b){return b-a;});
+	graalMults = [...new Set(graalMults)]
+	graalMults.sort(function(a,b){return b-a;});
+	baseMults[0] *= set_coeff[SetType];
+	graalMults[0] *= set_coeff[SetType];
 	let baseStr = "";
 	let graalStr = "";
-	for (var i = 0; i < 4; i++){
-		baseStr += String(baseMults[i]) + ",";
-		graalStr += String(graalMults[i]) + ",";
+	for (var i = 0; i < baseMults.length-1; i++){
+		baseStr += String(Math.ceil(baseMults[i])) + ",";
+		graalStr += String(Math.ceil(graalMults[i])) + ",";
 	}
-	baseStr += String(baseMults[4]);
-	graalStr += String(graalMults[4]);
+	baseStr += String(Math.ceil(baseMults[baseMults.length-1]));
+	graalStr += String(Math.ceil(graalMults[graalMults.length -1]));
 	document.getElementById("baseMults").value = baseStr;
 	document.getElementById("graalMults").value = graalStr;
 }
 
-function setEpikNum() {
-  epikCounter = parseInt(document.getElementById('epikNum').value);
+function calcMults(curr,lev){
+	for (var i = 0; i < 2; ++i){
+		if (i == 0){
+			if (lev < 3) calcMults(curr,lev+1);
+			else {
+				baseMults.push(curr);
+				graalMults.push(curr*(1.0 + parseFloat(document.getElementById('graal').value)/100.0));
+			}
+		}
+		else{
+			let coeff = lev_coeff[SetType][parseInt(document.getElementById(names[lev]).value)-1];
+			if (lev < 3)  calcMults(coeff*curr,lev+1);
+			else {
+				baseMults.push(coeff*curr);
+				graalMults.push(coeff*curr*(1.0 + parseFloat(document.getElementById('graal').value)/100.0));
+			}
+		}
+	}
 }
 
-function setEpikNum() {
-  epikCounter = parseInt(document.getElementById('mythNum').value);
-}
-function downBonus(set){
-  if (set == 1){
-	let currVal = parseInt(document.getElementById('epikNum').value) ;
-	let minVal = parseInt(document.getElementById('epikNum').min);
-	if (currVal > minVal ) document.getElementById('epikNum').value = currVal - 1;
-	epikCounter = parseInt(document.getElementById('epikNum').value);
-  }else if (set == 2){
-	let currVal = parseInt(document.getElementById('mythNum').value) ;
-	let minVal = parseInt(document.getElementById('mythNum').min);
-	if (currVal > minVal ) document.getElementById('mythNum').value = currVal - 1;
-	mythCounter = parseInt(document.getElementById('mythNum').value);
-  }else if (set == 3){
-	let currVal = parseInt(document.getElementById('oneNum').value) ;
-	let minVal = parseInt(document.getElementById('oneNum').min);
-	if (currVal > minVal ) document.getElementById('oneNum').value = currVal - 1;
-	onePctCounter = parseInt(document.getElementById('oneNum').value);
-  }else if (set == 4){
-	let currVal = parseInt(document.getElementById('twoNum').value) ;
-	let minVal = parseInt(document.getElementById('twoNum').min);
-	if (currVal > minVal ) document.getElementById('twoNum').value = currVal - 1;
-	twoPctCounter = parseInt(document.getElementById('twoNum').value);
-  }
-  clear_mults();
-}
-
-function upBonus(set){
-  if (set == 1){
-	let currVal = parseInt(document.getElementById('epikNum').value) ;
-	let maxVal = parseInt(document.getElementById('epikNum').max);
-	if (currVal < maxVal ) document.getElementById('epikNum').value = currVal + 1;
-	epikCounter = parseInt(document.getElementById('epikNum').value);
-  }else  if(set == 2){
-	let currVal = parseInt(document.getElementById('mythNum').value) ;
-	let maxVal = parseInt(document.getElementById('mythNum').max);
-	if (currVal < maxVal ) document.getElementById('mythNum').value = currVal + 1;
-	mythCounter = parseInt(document.getElementById('mythNum').value);
-  }else if(set == 3){
-	let currVal = parseInt(document.getElementById('oneNum').value) ;
-	let maxVal = parseInt(document.getElementById('oneNum').max);
-	if (currVal < maxVal ) document.getElementById('oneNum').value = currVal + 1;
-	onePctCounter = parseInt(document.getElementById('oneNum').value);
-  }else  if(set == 4){
-	let currVal = parseInt(document.getElementById('twoNum').value) ;
-	let maxVal = parseInt(document.getElementById('twoNum').max);
-	if (currVal < maxVal ) document.getElementById('twoNum').value = currVal + 1;
-	twoPctCounter = parseInt(document.getElementById('twoNum').value);
-  }
-  clear_mults();
+function calcMult(curr){
+	let allChk = true;
+	let mult = curr;
+	for (var i = 0; i < 4; i++){
+		let chkStr = "chk" + String(i+1);
+		if (document.getElementById(chkStr).checked){
+			mult *= lev_coeff[SetType][parseInt(document.getElementById(names[i]).value)-1];
+		}
+		else{
+			allChk = false;
+		}
+	}
+	if (allChk){
+		mult *= set_coeff[SetType];
+	}
+	return mult;
 }
 
 function clear_mults() {
 	document.getElementById("baseMults").value = "";
 	document.getElementById("graalMults").value = "";
+}
+
+function comb() {
+	let rate    = parseFloat(document.getElementById('rate').value)/1000.0;
+	let level   = parseFloat(document.getElementById('level').value);
+	let library = parseFloat(document.getElementById('library').value);
+	let graal   = 1.0 + parseFloat(document.getElementById('graal').value)/100.0;
+	epikCounter   = parseInt(document.getElementById('epikNum').value);
+	mythCounter   = parseInt(document.getElementById('mythNum').value);
+	onePctCounter = parseInt(document.getElementById('oneNum').value);
+	twoPctCounter = parseInt(document.getElementById('twoNum').value);
+	let coeff = SetType == 0 ? 1.4 : 1.3;
+	let currMult = rate*level*library*Math.pow(1.05,epikCounter)*
+			Math.pow(1.07,mythCounter)*Math.pow(1.01,onePctCounter)*Math.pow(1.02,twoPctCounter);
+	currMult = calcMult(currMult);
+	let baseStr = String(Math.ceil(currMult));
+	let graalStr = String(Math.ceil(currMult*(1.0 + parseFloat(document.getElementById('graal').value)/100.0)));
+	document.getElementById("baseMults").value = baseStr;
+	document.getElementById("graalMults").value = graalStr;
 }
