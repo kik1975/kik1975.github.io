76babca0a3f1803763326a0679935da3db899a50
kik1975 <44049549+kik1975@users.noreply.github.com>
2023-02-21 18:49:07 +0700

    Update settings.js

--- Special/Gamers/Yulya/settings.js
+++ Special/Gamers/Yulya/settings.js
@@ -5,6 +5,6 @@ var baseStr = ["8598,6450,3233","5829,4404","",""];
 var accNames = ["Юля","Валек","",""];
 var accs = 2
 var begDates = [[2022, 9, 29],[2022, 9, 29],[2021, 2, 31],[2021, 2, 31]];
-var range = [1275,1275,133,133];
+var range = [1276,1276,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,true,false,false];
