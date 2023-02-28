7eea148d4d5908f87a71da3d0e11792af2f83500
kik1975 <44049549+kik1975@users.noreply.github.com>
2023-02-28 20:33:53 +0700

    Update settings.js

--- Special/Gamers/Olya/settings.js
+++ Special/Gamers/Olya/settings.js
@@ -1,7 +1,7 @@
 var videoBonuses = [12,12,6,12];
 var code_num = [3,1,3,3];
 var baseStr = ["3150,2475,2269,676","","",""];
-var graalStr = ["","","",""];
+var graalStr = ["3622,2846,2609,777","","",""];
 var accNames = ["","","",""];
 var accs = 1
 var begDates = [[2022, 9, 29],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
