a4977ebdac10e64d38a3d782b25f4a86057701eb
wordcalc <130208659+wordcalc@users.noreply.github.com>
2023-04-13 19:39:11 +0700

    множ

--- Special/Random/Andrew/settings.js
+++ Special/Random/Andrew/settings.js
@@ -1,10 +1,11 @@
 var videoBonuses = [13,12,6,12];
 var code_num = [3,1,3,3];
-var baseStr = ["3433,2019,1995,1425,1408,1006,994,702","","",""];
-var graalStr = ["3947,2322,2294,1639,1620,1157,1143,807","","",""];
+var baseStr = ["3741,2201,2175,1554,1535,1097,1084,765","","",""];
+var graalStr = ["4303,2531,2501,1786,1765,1261,1246,880","","",""];
 var accNames = ["","","",""];
 var accs = 1
 var begDates = [[2023, 2, 31],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
 var range = [1007,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
+ 
