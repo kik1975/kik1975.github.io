588a497b1a38f28c4de781c3a97cd1da5668bd4a
kik1975 <44049549+kik1975@users.noreply.github.com>
2023-05-10 20:05:04 +0700

    Update settings.js

--- Special/Gamers/Eugenia/settings.js
+++ Special/Gamers/Eugenia/settings.js
@@ -4,6 +4,6 @@ var graalStr = ["1163,1627,2278,3189,5358","","",""];
 var accNames = ["","","",""];
 var accs = 1
 var begDates = [[2023, 4, 10],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [1248,133,133,133];
+var range = [1249,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
