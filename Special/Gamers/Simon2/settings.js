f90a10affb44e6d32e5a80eb2ef293cd5836dcb7
kik1975 <44049549+kik1975@users.noreply.github.com>
2024-03-10 20:29:50 +0700

    Update settings.js

--- Special/Gamers/Simon2/settings.js
+++ Special/Gamers/Simon2/settings.js
@@ -5,6 +5,6 @@ var graalStr = ["","","",""];
 var accNames = ["","","",""];
 var accs = 1;
 var begDates = [[2023, 4, 10],[2021, 2, 31],[2021, 4, 23],[2021, 2, 31]];
-var range = [1249,133,292,133];
+var range = [1250,133,292,133];
 var isLiteVersion = true;
 var isAutoLevel = [true,false,false,false];
