7b9c796590ff21aa0bf64fffe5f5af4d7433ab58
kik1975 <44049549+kik1975@users.noreply.github.com>
2024-06-01 11:44:59 +0700

    Update settings.js

--- Special/Gamers/Yulya/settings.js
+++ Special/Gamers/Yulya/settings.js
@@ -1,7 +1,7 @@
 var videoBonuses = [13,13,6,12];
 var code_num = [3,3,3,3];
 var graalStr = ["7648,5536,4535","","",""];
-var baseStr = ["","","",""];
+var baseStr = ["6651,4814","","",""];
 var accNames = ["Юля","Валек","",""];
 var accs = 2
 var begDates = [[2024, 3, 24],[2023, 4, 10],[2021, 2, 31],[2021, 2, 31]];
