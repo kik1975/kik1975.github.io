3ed50bbca96138f29dd2ba803ff512190a307cf2
kik1975 <kik@ngs.ru>
2024-06-17 12:56:00 +0700

    Update settings.js

--- Special/Gamers/Kik/settings.js
+++ Special/Gamers/Kik/settings.js
@@ -1,5 +1,5 @@
 var isLiteVersion = false;
-var graalStr = ["6212 3525 2401 1635 1114","","",""];
+var graalStr = ["6286 3568 2430 1655 1114","","",""];
 var accs = 1;
 var accNames = ["ВКонтакте","Google","",""];
 var baseStr = ["5402 3066 2088 1422 969","","",""];
