ac1a0f1ca0326da41c8526b9c340b236ae4d0315
kik1975 <kik@ngs.ru>
2025-01-09 11:11:38 +0700

    Update settings.js

--- Special/Gamers/Kik/settings.js
+++ Special/Gamers/Kik/settings.js
@@ -1,8 +1,8 @@
 var isLiteVersion = false;
-var graalStr = ["8914 5059 3404 2291 1542","7493 4202 2828 1881 1251","",""];
+var graalStr = ["6572 3730 2510 1689 1137","5662 3175 2137 1421 945","",""];
 var accs = 2;
 var accNames = ["ВКонтакте","Юля","",""];
-var baseStr = ["7751 4399 2960 1992 1341","6516 3654 2459 1635 1088","",""];
+var baseStr = ["","","",""];
 var videoBonuses = [13,13,12,12];
 var isAutoLevel = [true,true,true,true];
 var begDates = [[2024, 10, 10],[2024, 10, 10],[2022, 5, 11],[2022, 5, 11]];
