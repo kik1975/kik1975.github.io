3fa9f5e4b98b38aa46fe0ae7f1d761874446ce47
kik1975 <kik@ngs.ru>
2025-03-14 10:12:16 +0700

    POnt
    Вариант для БЕЗ ПОНТОВ

---
+++ Special/Gamers/Pont/settings.js
@@ -0,0 +1,10 @@
+var videoBonuses = [13,13,6,12];
+var baseStr = ["БЕЗ ПОНТОВ","БАЗОВЫЙ","",""];
+var graalStr = ["","","",""];
+var code_num = [3,3,3,3];
+var accNames = ["","","",""];
+var accs = 2
+var begDates = [[2024, 10, 10],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
+var range = [803,133,133,133];
+var isLiteVersion = false;
+var isAutoLevel = [true,false,false,false];
