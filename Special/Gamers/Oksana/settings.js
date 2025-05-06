8afdcd003eb0bff889abcc29640047c2b7c0d2ea
kik1975 <kik@ngs.ru>
2025-05-06 15:16:39 +0700

    Update settings.js

--- Special/Gamers/Oksana/settings.js
+++ Special/Gamers/Oksana/settings.js
@@ -1,9 +1,10 @@
-var videoBonuses = [12,12,6,12];
-var baseStr = ["602,843,1180,1651,2774","","",""];
-var graalStr = ["692,969,1356,1899,3189","","",""];
+var videoBonuses = [13,12,6,12];
+var baseStr = ["","","",""];
+var graalStr = ["","","",""];
 var accNames = ["","","",""];
 var accs = 1
-var begDates = [[2022, 5, 11],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
-var range = [854,133,133,133];
+var begDates = [[2024, 10, 10],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
+var range = [803,133,133,133];
 var isLiteVersion = false;
 var isAutoLevel = [true,false,false,false];
+var code_num = [3,3,3,2];
