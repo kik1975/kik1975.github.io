d95cd2f1445a16d14b2bd2bc280c8c1780955db0
kik1975 <44049549+kik1975@users.noreply.github.com>
2025-06-10 13:18:12 +0700

    Update settings.js

--- Special/Gamers/Christina/settings.js
+++ Special/Gamers/Christina/settings.js
@@ -1,6 +1,6 @@
 var videoBonuses = [13,12,6,12];
 var baseStr = ["","","",""];
-var graalStr = ["","","",""];
+var graalStr = ["2262,1284,875,596,406","","",""];
 var accNames = ["","","",""];
 var accs = 1
 var begDates = [[2025, 5, 1],[2021, 2, 31],[2021, 2, 31],[2021, 2, 31]];
